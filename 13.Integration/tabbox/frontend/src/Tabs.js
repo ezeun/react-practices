411c451414f46a4cfccc1f855c4ef538d244f3f9
ezeun <consistency1231@gmail.com>
2025-02-10 17:05:53 +0900

    13.tabbox 과제

---
+++ 13.Integration/tabbox/frontend/src/Tabs.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import TabItem from './TabItem';
+
+function Tabs({tabs}) {
+    return (
+        <ul>
+            {
+                tabs.map(tab => <TabItem 
+                                        key = {tab.no}
+                                        name={tab.name}
+                                        active={tab.active}
+                                />)
+            }
+        </ul>
+    );
+}
+
+export default Tabs;
