b97e809e8ae2ab106f351200a9c8e1f967c7d5a2
ezeun <consistency1231@gmail.com>
2025-02-20 14:03:08 +0900

    ex05

--- 09.Routing/ex05/src/component/Error404.js
+++ 09.Routing/ex05/src/component/Error404.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Header from "../layout/Header";
+import Navigation from "../layout/Navigation";
+import Footer from "../layout/Footer";
+import * as styles from '../assets/scss/component/Guestbook.scss';
+
 
 export default function Error404() {
     return (
         <>
+            <Header/>
+            <div className={styles.Error404}>
+                <h2>Error404</h2>
+            </div>
+            <Navigation/>
+            <Footer/>
         </>
     );
 }
