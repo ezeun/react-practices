96e15faa56aa62043a8d9aac0ca689ca62c3be08
ezeun <consistency1231@gmail.com>
2025-02-20 10:20:49 +0900

    ex01

--- 09.Routing/ex01/src/App.js
+++ 09.Routing/ex01/src/App.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Main from "./component/Main";
 import Gallery from "./component/Gallery";
 import Guestbook from "./component/Guestbook";
 import Error404 from "./component/Error404";
 
 export default function App() {
-    return (() => {
-        switch(route) {
-            case '' :
-            case '/' :
-                return <Main />;
-            case '/guestbook' :
-                return <Guestbook />;
-            case '/gallery':
-                return <Gallery />;
-            default :
-                return <Error404 />;                    
-        }
-    })();
+    const [route, setRoute] = useState('');
+
+    const hashchangeHandler = () => {
+        console.log(window.location.hash);
+        setRoute(window.location.hash.substring(1));
+    }
+
+    useEffect(() => {
+        window.addEventListener('hashchange', hashchangeHandler);
+
+        return () => {
+            window.removeEventListener('hashchange', hashchangeHandler);
+        };
+    }, []);
+
+    return (
+        <>
+            <ul>
+                <li><a href="/#/">main</a></li>
+                <li><a href="/#/guestbook">guestbook</a></li>
+                <li><a href="/#/gallery">gallery</a></li>
+            </ul>
+            {
+                (() => {
+                    switch(route) {
+                        case '' :
+                        case '/' :
+                            return <Main />;
+                        case '/guestbook' :
+                            return <Guestbook />;
+                        case '/gallery':
+                            return <Gallery />;
+                        default :
+                            return <Error404 />;                    
+                    }
+                })()
+            }
+        </>
+    )
 }
