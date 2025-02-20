fd0405446e8ec68652eed611be6ac8832fc26b7c
ezeun <consistency1231@gmail.com>
2025-02-20 13:07:46 +0900

    ex03

--- 09.Routing/ex03/src/App.js
+++ 09.Routing/ex03/src/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import {HashRouter, BrowserRouter} from 'react-router-dom';
+import {Routes, Route} from 'react-router';
+import Main from './component/Main';
+import Gallery from "./component/Gallery";
+import Guestbook from "./component/Guestbook";
+import Error404 from "./component/Error404";
 
 export default function App() {
     return (
-        <></>
+        /*/
+        <HashRouter>
+            <Routes>
+                <Route path={'/'} element={<Main />}/>
+                <Route path={'/guestbook'} element={<Guestbook />}/>
+                <Route path={'/gallery'} element={<Gallery />}/>
+                <Route path={'*'} element={<Error404 />}/>
+            </Routes>
+        </HashRouter>
+
+        /*/
+
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<Main />}/>
+                <Route path={'/guestbook'} element={<Guestbook />}/>
+                <Route path={'/gallery'} element={<Gallery />}/>
+                <Route path={'*'} element={<Error404 />}/>
+            </Routes>
+        </BrowserRouter>
+
+        //*/
     );
 }
