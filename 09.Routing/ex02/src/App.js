fb0ee586631ffcab83b3e31ecc2fab2683ecb3f1
ezeun <consistency1231@gmail.com>
2025-02-20 11:36:09 +0900

    ex02

--- 09.Routing/ex02/src/App.js
+++ 09.Routing/ex02/src/App.js
@@ -2,20 +2,57 @@ import React, {useState, useEffect} from 'react';
 import Main from "./component/Main";
 import Gallery from "./component/Gallery";
 import Guestbook from "./component/Guestbook";
-
-
+import Error404 from "./component/Error404";
 
 const Router = () => {
-    return null;
+    const [route, setRoute] = useState({page: window.location.href.substring(window.location.href.lastIndexOf('/'))});
+
+    const pushstateHandler = (e) => {
+        setRoute(e.detail ? e.detail : {page: '/'});
+    };
+
+    const popstateHandler = (e) => {
+        setRoute(e.state ? e.state : {page: '/'});
+    }
+
+    useEffect(() => {
+        window.addEventListener('pushstate', pushstateHandler);
+        window.addEventListener('popstate', popstateHandler);
+
+        return () => {
+            window.removeEventListener('pushstate', pushstateHandler);
+            window.removeEventListener('popstate', popstateHandler);
+        };
+    }, []);
+
+    let component = null;
+
+    switch(route.page) {
+        case '' :
+        case '/' :
+            component = <Main />;
+            break;
+        case '/guestbook' :
+            component = <Guestbook />;
+            break;
+        case '/gallery':
+            component = <Gallery />;
+            break;
+        default :
+            component = <Error404 />;                    
+    }
+
+    return component;
 };
-
-
-
 
 export default function App() {
 
     const clickHandler = (e) => {
         e.preventDefault();
+
+        const url = e.target.href.substring(e.target.href.lastIndexOf("/"));
+        window.history.pushState({page: url}, e.target.txt, url);
+        window.dispatchEvent(new CustomEvent('pushstate', {detail: {page: url}}));
     }
 
     return (
